c9c67b662cbcd362d736a1509e7c05fae4c24334
tuva9 <tuva9@fpt.edu.vn>
2023-04-16 09:26:48 +0700

    Add google analytics

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://vuanhtu1993.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/vuanhtu1993.github.io',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -54,6 +54,10 @@ const config = {
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
+        },
+        gtag: {
+          trackingID: 'G-GQETJJQ2L9',
+          anonymizeIP: true,
         },
       }),
     ],
