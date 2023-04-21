96abadbc21d50d7ef2ea9942601ecd9b084c71a6
tuva9 <tuva9@fpt.edu.vn>
2023-04-21 16:52:30 +0700

    Deploy website - based on f68ee4005ac0e0ec4a032d97a791f6dc9eecd520

---
+++ assets/js/9dd8a0d2.c6661ca9.js
@@ -0,0 +1,41 @@
+"use strict";(self.webpackChunkstory=self.webpackChunkstory||[]).push([[7054],{7275:(t,n,e)=>{e.r(n),e.d(n,{default:()=>o});var i=e(7294),a=(e(9960),e(2263)),l=e(7961),h=e(9521),c=e(9911);function o(){const{siteConfig:t}=(0,a.Z)(),n=(0,i.useRef)(null);return(0,i.useEffect)((()=>{const t=new c.Z(n.current,{strings:["T\xf4i l\xe0 fullstack developer","T\xf4i y\xeau c\xf4ng ngh\u1ec7, \u0111\u1eb7c bi\u1ec7t l\xe0 AI v\xe0 Blockchain"],startDelay:300,typeSpeed:100,backSpeed:100,backDelay:100,loop:!0});return()=>{t.destroy()}}),[]),i.createElement(l.Z,{title:"Anhhtus stories",description:"Description will go into a meta tag in <head />"},i.createElement(r,{width:"1200px",className:""},i.createElement("div",{className:"w-full"},i.createElement(m,null,"Ch\xe0o m\u1eebng \u0111\u1ebfn v\u1edbi c\xe2u chuy\u1ec7n c\u1ee7a"),i.createElement(g,null,"V\u0169 Anh T\xfa"),i.createElement("span",{style:{fontFamily:"Ysabeau",fontSize:20,fontWeight:"bold",color:"#616d81"},ref:n}),i.createElement("div",{className:"mt-2"}),i.createElement(u,null,"T\xf4i c\xf3 s\u1ef1 t\xf2 m\xf2 v\xf4 h\u1ea1n v\u1ec1 AI, h\u01a1n h\u1ebft l\xe0 Deep Learning. T\xf4i \u0111am m\xea t\u1ea1o ra c\xe1c \u1ee9ng d\u1ee5ng th\xf4ng minh \u0111\u1ec3 ph\u1ee5c v\u1ee5 con ng\u01b0\u1eddi trong nhi\u1ec1u l\u0129nh v\u1ef1c"),i.createElement(u,null,"T\xf4i th\xe0nh th\u1ea1o l\u1eadp tr\xecnh web b\u1eb1ng Javascript, Python. T\xf4i c\xf3 nhi\u1ec1u kinh nghi\u1ec7m v\u1edbi OOP v\xe0 design pattern. Ki\u1ebfn th\u1ee9c v\u1ec1 c\u1ea5u tr\xfac d\u1eef li\u1ec7u v\xe0 gi\u1ea3i thu\u1eadt, ph\xe2n t\xedch thi\u1ebft k\u1ebf d\u1ef1 \xe1n"),i.createElement(u,null,"T\xf4i c\xf3 k\u1ef9 n\u0103ng gi\u1ea3i quy\u1ebft v\u1ea5n \u0111\u1ec1, kh\u1ea3 n\u0103ng t\u1ef1 h\u1ecdc, t\u1ef1 nghi\xean c\u1ee9u r\u1ea5t cao. Nh\u1edd \u0111am m\xea c\xf4ng ngh\u1ec7 v\xe0 th\xe0nh th\u1ea1o \u0111\u1ecdc hi\u1ec3u t\xe0i li\u1ec7u ti\u1ebfng anh t\xf4i th\u01b0\u1eddng xuy\xean ti\u1ebfp c\u1eadn ki\u1ebfn th\u1ee9c m\u1edbi v\u1ec1 l\u1eadp tr\xecnh \u1ee9ng d\u1ee5ng v\xe0 AI")),i.createElement(s,{src:"img/anhhtus.svg"})))}const r=h.ZP.div`
+  width: 100%;
+  display: block;
+  margin: 0 auto;
+  padding: 16px;
+  @media (min-width: 1024px) {
+    width: ${t=>t.width||"1400px"};
+    display: grid;
+    grid-template-columns: 2fr 1fr;
+    gap: 16px;
+    margin-top: 48px;
+  }
+`,s=h.ZP.img`
+  width: 250px;
+  height: auto;
+  display: block;
+  margin: 0 auto;
+  @media (min-width: 1024px) {
+    
+  }
+`,g=h.ZP.h1`
+  font-family: Ysabeau;
+  font-size: 60px;
+  font-weight: bolder;
+  color: #388967
+`,m=h.ZP.h2`
+font-size: 20px;
+  font-family: Ysabeau;
+  font-weight: normal;
+`,u=h.ZP.p`
+font-family: Ysabeau;
+font-size: 20px;
+`;(0,h.ZP)(c.Z)`
+  font-family: Ysabeau !important;
+  font-size: 20px;
+  font-weight: bold;
+`,h.ZP.img`
+  width: 100%;
+  height: 500px;
+  object-fit: cover;
+`}}]);
